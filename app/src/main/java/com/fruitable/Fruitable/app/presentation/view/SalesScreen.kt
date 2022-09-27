ba2f2e6c58c905b3c2aa1d5deea04334af3c417d
WoeunJung <leena09123@naver.com>
2022-09-27 23:56:53 +0900

    style: add plus Button on SalesScreen

--- app/src/main/java/com/fruitable/Fruitable/app/presentation/view/SalesScreen.kt
+++ app/src/main/java/com/fruitable/Fruitable/app/presentation/view/SalesScreen.kt
@@ -1,17 +1,15 @@
 package com.fruitable.Fruitable.app.presentation.view
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import android.widget.ImageButton
+import androidx.compose.foundation.*
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Divider
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
@@ -22,14 +20,17 @@ import androidx.compose.ui.Alignment.Companion.End
 import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.fruitable.Fruitable.R
 import com.fruitable.Fruitable.app._enums.HashTag
 import com.fruitable.Fruitable.app.presentation.component.HashTagButton
 import com.fruitable.Fruitable.app.presentation.component.ProfileImage
@@ -42,32 +43,53 @@ import com.fruitable.Fruitable.ui.theme.TextStyles
 fun SalesScreen(
     navController: NavController
 ){
-
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MainGreen1)
-    ){
-        item {
-            Column(
+    Scaffold(
+        floatingActionButton = {
+            Button(
+                onClick = {},
                 modifier = Modifier
-                    .fillMaxWidth()
-            ) {
-                SellerProfile(
-                    modifier = Modifier
-                        .align(End)
-                        .padding(0.dp, 47.dp, 30.dp, 19.dp)
+                    .padding(9.dp)
+                    .size(56.dp)
+                    .shadow(5.dp, shape = CircleShape)
+                    .clip(CircleShape),
+                colors = ButtonDefaults.buttonColors(MainGreen1),
+                contentPadding = PaddingValues(0.dp)
+            ){
+                Image(
+                    painterResource(id = R.drawable.plusbtn),
+                    contentDescription = "add_post",
+                    modifier = Modifier.size(24.dp)
                 )
             }
         }
-        item {
-            SalesContents(
-                modifier = Modifier
-                    .clip(RoundedCornerShape(30.dp, 30.dp, 0.dp, 0.dp))
-                    .background(White)
-                    .fillMaxSize()
-            )
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MainGreen1)
+        ) {
+            item {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    SellerProfile(
+                        modifier = Modifier
+                            .align(End)
+                            .padding(0.dp, 47.dp, 30.dp, 19.dp)
+                    )
+                }
+            }
+            item {
+                SalesContents(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(30.dp, 30.dp, 0.dp, 0.dp))
+                        .background(White)
+                        .fillMaxSize()
+                )
+            }
         }
+       it
     }
 }
 
