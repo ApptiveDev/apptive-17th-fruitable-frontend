15b7fa0bb843e086723945c73a879fe5213da7f0
WoeunJung <leena09123@naver.com>
2022-09-27 22:28:51 +0900

    style: change mainGreen colors

--- app/src/main/java/com/fruitable/Fruitable/ui/theme/Color.kt
+++ app/src/main/java/com/fruitable/Fruitable/ui/theme/Color.kt
@@ -2,9 +2,9 @@ package com.fruitable.Fruitable.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val MainGreen1 = Color(0xFF229918)
-val MainGreen2 = Color(0xFFD7DFDA)
-val MainGreen3 = Color(0xFFF2FAF5)
+val MainGreen1 = Color(0xFF0DB159)
+val MainGreen2 = Color(0xFFF2FAF5)
+val MainGreen3 = Color(0xFFD7DFDA)
 
 val MainGray1 = Color(0xFF757575)
 val MainGray2 = Color(0xFF8C8585)
