d0c7d504adabf29e6994653f0b05d4808473d738
WoeunJung <leena09123@naver.com>
2022-12-24 13:40:49 +0900

    style: make SettingScreen

---
+++ app/src/main/java/com/fruitable/Fruitable/app/presentation/view/setting/LeaveAppScreen.kt
@@ -0,0 +1,11 @@
+package com.fruitable.Fruitable.app.presentation.view.setting
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun LeaveAppScreen(
+    navController: NavController
+){
+
+}
---
+++ app/src/main/java/com/fruitable/Fruitable/app/presentation/view/setting/UpgradingScreen.kt
@@ -0,0 +1,11 @@
+package com.fruitable.Fruitable.app.presentation.view.setting
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun UpgradingScreen(
+    navController: NavController
+){
+
+}
