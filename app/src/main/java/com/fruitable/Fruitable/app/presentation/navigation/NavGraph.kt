129a5d49b488db90d2ca04d25bce52b39be21804
WoeunJung <leena09123@naver.com>
2022-12-24 13:40:04 +0900

    feat: define NavGraph and Screen in Setting

--- app/src/main/java/com/fruitable/Fruitable/app/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/fruitable/Fruitable/app/presentation/navigation/NavGraph.kt
@@ -1,13 +1,18 @@
 package com.fruitable.Fruitable.app.presentation.navigation
 
+import android.accounts.Account
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.fruitable.Fruitable.app.presentation.view.*
+import com.fruitable.Fruitable.app.presentation.view.setting.*
 
 fun NavGraphBuilder.fruitableGraph(
     navController: NavController
 ){
+    /**
+     * sale - 전체 게시글, 상세 게시글, 게시글 추가 Screen
+     */
     composable(
         route = Screen.SalesScreen.route) {
         SalesScreen(navController)
@@ -20,6 +25,9 @@ fun NavGraphBuilder.fruitableGraph(
         route = Screen.DetailSalesScreen.route+"/{itemId}"){ backStackEntry ->
         DetailSalesScreen(navController = navController, itemId = (backStackEntry.arguments?.getInt("itemId") ?: "") as Int)
     }
+    /**
+     * user - 로그인, 회원가입, 회원정보 수정 Screen
+     */
     composable(
         route = Screen.SignInScreen.route){
             SignInScreen(navController)
@@ -32,5 +40,28 @@ fun NavGraphBuilder.fruitableGraph(
     composable(
         route = Screen.UserInfoUpdateScreen.route) {
         UserInfoUpdateScreen(navController)
+    }
+    /**
+     * setting - 설정, 계정 정보, 이메일 정보, 판매자 등업, 회원 탈퇴 Screen
+     */
+    composable(
+        route = Screen.AccountScreen.route) {
+        AccountScreen(navController)
+    }
+    composable(
+        route = Screen.SettingScreen.route) {
+        SettingScreen(navController)
+    }
+    composable(
+        route = Screen.UpgradingScreen.route) {
+        UpgradingScreen(navController)
+    }
+    composable(
+        route = Screen.EmailScreen.route) {
+        EmailScreen(navController)
+    }
+    composable(
+        route = Screen.LeaveAppScreen.route) {
+        LeaveAppScreen(navController)
     }
 }
--- app/src/main/java/com/fruitable/Fruitable/app/presentation/navigation/Screen.kt
+++ app/src/main/java/com/fruitable/Fruitable/app/presentation/navigation/Screen.kt
@@ -1,10 +1,25 @@
 package com.fruitable.Fruitable.app.presentation.navigation
 
 sealed class Screen (val route: String){
+    /**
+     * sale - 전체 게시글, 상세 게시글, 게시글 추가 Screen
+     */
     object SalesScreen: Screen("sales_screen")
     object DetailSalesScreen : Screen("detail_sales_screen/{itemId}")
     object AddSaleScreen: Screen("add_sale_screen")
+
+    /**
+     * user - 로그인, 회원가입, 회원정보 수정 Screen
+     */
     object SignInScreen : Screen("sign_in_screen")
     object SingUpScreen : Screen("sign_up_screen")
-    object UserInfoUpdateScreen : Screen("user_info_update")
+    object UserInfoUpdateScreen : Screen("user_info_update_screen")
+    /**
+     * setting - 설정, 계정 정보, 이메일 정보, 판매자 등업, 회원 탈퇴 Screen
+     */
+    object SettingScreen : Screen("setting_screen")
+    object AccountScreen : Screen("account_screen")
+    object UpgradingScreen : Screen("upgrading_screen")
+    object EmailScreen: Screen("email_screen")
+    object LeaveAppScreen: Screen("leave_app_screen")
 }
