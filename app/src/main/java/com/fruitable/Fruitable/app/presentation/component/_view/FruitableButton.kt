d8a649312447590a20f301a68bfc9920e9748765
WoeunJung <leena09123@naver.com>
2022-12-25 22:43:15 +0900

    refactor: revise button component

--- app/src/main/java/com/fruitable/Fruitable/app/presentation/component/_view/FruitableButton.kt
+++ app/src/main/java/com/fruitable/Fruitable/app/presentation/component/_view/FruitableButton.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.fruitable.Fruitable.app.domain.utils.NoRippleInteractionSource
 import com.fruitable.Fruitable.ui.theme.*
@@ -27,14 +28,15 @@ fun FruitableButton(
     style: TextStyle = TextStyles.TextBasic3,
     isCancellable: Boolean = false,
     color: Color = Color.White,
+    cornerShape: Dp = 10.dp,
     textColor: Color = MainGreen1,
 ){
     val buttonColor = ButtonDefaults.buttonColors(color)
     Button(
         onClick = onClick,
         modifier = modifier
-            .clip(RoundedCornerShape(10.dp))
-            .border(1.dp,textColor,RoundedCornerShape(10.dp)),
+            .clip(RoundedCornerShape(cornerShape))
+            .border(1.dp,textColor,RoundedCornerShape(cornerShape)),
         contentPadding = if (isCancellable) PaddingValues(start = 9.dp, end = 12.dp,top=0.dp,bottom=0.dp)
                          else PaddingValues(horizontal = 15.dp),
         colors = buttonColor,
