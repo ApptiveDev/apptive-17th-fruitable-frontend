45f865503bf961faa78af61d1744bdc78795589d
WoeunJung <leena09123@naver.com>
2023-01-14 00:15:32 +0900

    refactor: change baseClass name

--- app/src/main/java/com/fruitable/Fruitable/app/data/network/dto/user/SignUpDTO.kt
+++ app/src/main/java/com/fruitable/Fruitable/app/data/network/dto/user/SignUpDTO.kt
@@ -1,8 +1,10 @@
 package com.fruitable.Fruitable.app.data.network.dto.user
 
+import com.fruitable.Fruitable.app.data.network.dto.BaseClass
+
 data class SignUpDTO(
     val email: String = "",
     val name: String = "",
     val pwd: String = "",
     val pwd2: String = ""
-): UserBaseClass()
+): BaseClass()
